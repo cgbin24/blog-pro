d61b1d8aec0d6cb835b99d5403b7e7e1d3551030
c <c_being@qq.com>
2023-01-18 11:29:53 +0800

    fix: 增加root节点挂载错误处理

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,12 @@ import React from 'react'
 import ReactDom from 'react-dom/client'
 import App from './App'
 
-const root = ReactDom.createRoot(
-  document.getElementById('root') as HTMLElement
-)
-root.render(<App />)
+const renderInBrowser = () => {
+  const rootEl = document.getElementById('root') as HTMLElement
+  if (!rootEl) {
+    throw new Error('#root element is not found')
+  }
+  const root = ReactDom.createRoot(rootEl)
+  root.render(<App />)
+}
+renderInBrowser()
